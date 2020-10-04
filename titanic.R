9ed3c007a59906223dfc1c98c8877fee2b278884
Igna <gnachoxp@gmail.com>
2020-10-03 21:55:45 -0300

    start project

---
+++ titanic.R
@@ -0,0 +1,11 @@
+###############
+# Prediction Model of Survivals in Titanic
+# Competition of Kaggle
+#
+# Creator : Igna Garcia
+# In representation of : Universidad Nacional del Oeste, Argentina
+#
+# Create date: 2020/10/03
+# Update date: 2020/10/30 
+#        comment: 
+###############
